0c374f75e7d1b08d12a222b96fee3d15d44c95aa
xionger <xunhao.xiong@gmail.com>
2018-07-29 21:41:52 -0500

    feat: implement questionsactivity

--- app/src/main/java/com/xiongxh/interviewpractice/base/PresenterProvider.java
+++ app/src/main/java/com/xiongxh/interviewpractice/base/PresenterProvider.java
@@ -3,15 +3,15 @@ package com.xiongxh.interviewpractice.base;
 
 import com.xiongxh.interviewpractice.Interviews.InterviewsContract;
 import com.xiongxh.interviewpractice.Interviews.InterviewsPresenter;
+import com.xiongxh.interviewpractice.questions.QuestionsContract;
+import com.xiongxh.interviewpractice.questions.QuestionsPresenter;
 
 public class PresenterProvider {
     public InterviewsContract.Presenter provideInterviews() {
         return new InterviewsPresenter();
     }
 
-    /*
-    public InterviewQuestionsContract.Presenter provideQuestions() {
-        return new InterviewQuestionsPresenter();
+    public QuestionsContract.Presenter provideQuestions() {
+        return new QuestionsPresenter();
     }
-    */
 }
--- app/src/main/java/com/xiongxh/interviewpractice/questions/QuestionsActivity.java
+++ app/src/main/java/com/xiongxh/interviewpractice/questions/QuestionsActivity.java
@@ -5,15 +5,41 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.xiongxh.interviewpractice.InterviewApp;
 import com.xiongxh.interviewpractice.R;
 
 public class QuestionsActivity extends AppCompatActivity {
     public static final String INTERVIEW_ID_KEY = "INTERVIEW_ID_KEY";
+    public static final String TAG_QUESTIONS_FRAGMENT = "TAG_QUESTIONS_FRAGMENT";
+
+    QuestionsFragment mQuestionsFragment;
+    int mInterviewId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
+
+        mInterviewId = getIntent().getIntExtra(INTERVIEW_ID_KEY, -1);
+
+        if (savedInstanceState == null) {
+            mQuestionsFragment = QuestionsFragment.newInstance(mInterviewId);
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.questions_container, mQuestionsFragment, TAG_QUESTIONS_FRAGMENT)
+                    .commit();
+        }
+
+        if (mQuestionsFragment == null){
+            mQuestionsFragment =
+                    (QuestionsFragment) getSupportFragmentManager().findFragmentByTag(TAG_QUESTIONS_FRAGMENT);
+        }
+
+        QuestionsContract.Presenter presenter = InterviewApp.get().presenterProvider.provideQuestions();
+
+        presenter.setInterviewId(mInterviewId);
+        mQuestionsFragment.setPresenter(presenter);
     }
 
     public static void onStartActivity(Context context, int interviewId){
